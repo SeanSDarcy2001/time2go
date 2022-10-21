234a60d5e33dd8d0f3a796031aa61ab8f2c46ecb
SeanSDarcy2001 <sdarcy2@jhu.edu>
2022-10-21 11:49:25 -0400

    button and accelerometer handled

--- functions/readSerial.py
+++ functions/readSerial.py
@@ -3,8 +3,8 @@ import numpy as np
 
 ser = serial.Serial('/dev/tty.HC-05-DevB', 9600) #, timeout=2, xonxoff=False, rtscts=False, dsrdtr=False) #Tried with and without the last 3 parameters, and also at 1Mbps, same happens.
 connected = True
-button = 'b'
-moving = 'n'
+button = -2
+moving = -1
 windowSize = 100
 
 def collectWindow(windowCollected) :
@@ -13,10 +13,15 @@ def collectWindow(windowCollected) :
         for i in range(windowSize) :
             bytesToRead = ser.readline()
             data = bytesToRead.decode('utf-8')
-            data = int(data.rstrip())
+            if data == moving :
+                y.append('moving')
+            elif data == button :
+                y.append(data[i - 1])
+            else :
+                data = int(data.rstrip())
             #print(data)
-            y.append(data)
-            i = i + 1
+                y.append(data)
+            i += 1
         windowCollected = True
     return y, windowCollected
 
